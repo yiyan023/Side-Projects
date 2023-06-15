fa635a6a78426edd6759ccc09583f8b4d4793f31
Yiyan Huang <56096857+yiyan023@users.noreply.github.com>
2023-06-15 10:20:12 -0400

    Create homepage.js

---
+++ LunarHacks/homepage.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick = function() {
+    
+    var fname = document.getElementById("myName").value;
+    var allergy = document.getElementById("myAllergy").value
+    console.log(fname);
+    console.log(allergy);
+}
