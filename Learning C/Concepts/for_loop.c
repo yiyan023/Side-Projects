6b5d7728397d8740be7c1843af6d669211f40c59
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-01-05 16:05:11 -0500

    For Loops

---
+++ Learning C/Concepts/for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int luckyNumbers[] = {4, 6, 8, 10, 12};
+    int Index;
+
+    for (Index = 0; Index < 5; Index++) {
+        printf("%d\n", luckyNumbers[Index]);
+    }
+}
