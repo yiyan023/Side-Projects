002807f9eee4b7e86da7cb8a33b765d83d5c3cc0
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-01-04 23:50:41 -0500

    Continuation of Functions & Return Statements

--- Learning C/Concepts/functions.c
+++ Learning C/Concepts/functions.c
@@ -5,7 +5,19 @@ void sayHi () {
     printf("Hello User!"); /* will NOT return anything unless you call it in main */
 }
 
-int main() {
-    sayHi();
+/* You can call parameters in a function */
+
+void sayHi2 (char Name[]) {
+    printf("Hello, %s!", Name);
 }
 
+double cube (double Num) {
+    double Result = Num * Num * Num;
+    return Result;
+}
+
+int main() {
+    sayHi();
+    sayHi2("Mike");
+    printf("%f", cube(3));
+}
