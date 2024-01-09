6c904b303648a30d64c0b93ac08307c7279a9e8e
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-01-08 23:46:01 -0500

    Learning to Execute a File in Java

---
+++ Learning-Java/Projects/Main.java
@@ -0,0 +1,7 @@
+// to run the file, cd Learning-Java, java Main -- will return "Hello World"
+
+public class Main { // you have to name your function the same file 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
