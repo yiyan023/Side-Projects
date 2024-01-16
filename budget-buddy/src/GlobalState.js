982f0c729d318dbf64d5f2d4a25255a4ff790759
Yiyan Huang <56096857+yiyan023@users.noreply.github.com>
2024-01-15 22:51:39 -0500

    Add files via upload

---
+++ budget-buddy/src/GlobalState.js
@@ -0,0 +1,37 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer'
+
+// Initial state
+const InitialState = {
+    transactions: []    
+}
+
+// Create context
+export const GlobalContext = createContext(InitialState);
+
+// provider component 
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, InitialState);
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
