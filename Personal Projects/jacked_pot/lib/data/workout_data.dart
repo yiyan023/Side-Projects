b70349fc03fe1f46f1d249cfd543dc1f66ae1b7a
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-01-31 00:12:51 -0500

    haha

--- Personal Projects/jacked_pot/lib/data/workout_data.dart
+++ Personal Projects/jacked_pot/lib/data/workout_data.dart
@@ -14,6 +14,35 @@ class WorkoutData {
           reps: "10",
         )
       ]
-    )
+    ),
   ];
+
+  List<Workout>getWorkoutList() {
+    return workoutList;
+  }
+
+  void addWorkout(String name) {
+    workoutList.add(Workout(name: name, exercises: []));
+  }
+
+  void addExercise(String workoutName, String exerciseName, String weight, String reps, String sets) {
+    Workout relevantWorkout = workoutList.firstWhere((workout) => workout.name == workoutName);
+
+    relevantWorkout.exercises.add(
+      Exercise(
+        name: exerciseName, 
+        weight: weight, 
+        sets: sets, 
+        reps: reps));
+  }
+
+  void checkOffExercise(String workoutName, String exerciseName) {
+
+  }
+
+  Workout getRelevantWorkout(String workoutName) {
+    Workout relevantWorkout = workoutList.firstWhere((workout) => workout.name == workoutName);
+
+    return relevantWorkout;
+  }
 }
