b1569fc26d6561b9cda036164ca2e360e7c251e1
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-02-06 15:58:57 -0500

    haha

---
+++ Learning Programming Languages/Learning C/CS136/Assignments/Assignment 3/q1b.c
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+// INTEGRITY STATEMENT (v4)
+//
+// By signing your name and ID below you are stating that you have agreed
+// to the online academic integrity statement posted on edX:
+// (Course > Assignments > Assignment Information & Policies > Academic Integrity Policy)
+/////////////////////////////////////////////////////////////////////////////////////////
+// I received help from and/or collaborated with:
+
+//
+// Name: Yiyan Huang
+// login ID: y84huang
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <assert.h>
+
+/* int main(void) {
+  char string = read_char(READ_WHITESPACE);
+  
+  while (string != READ_CHAR_FAIL) {
+    if (string >= 'a' && string <= 'z') {
+      string = string - 'a' + 'A';
+      printf("%c", string);
+      string = read_char(READ_WHITESPACE);
+    } else {
+      printf("%c", string);
+      //string = read_char(READ_WHITESPACE);
+    }
+  } 
+} */
