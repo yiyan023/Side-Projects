359e4043f86229068a3da2dcc5673f00eb8c24d4
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-02-11 00:11:52 -0500

    changed directories of files

--- Hackathons/DeltaHacks10/main.py
+++
@@ -1,8 +0,0 @@
-from website import create_app
-
-app = create_app()
-
-app.secret_key = "DeltaHacks10"
-
-if __name__ == '__main__':
-    app.run(debug=True) #rerun our code everytime we make a change
