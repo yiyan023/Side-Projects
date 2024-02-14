c41e598965e65d3c510ae81df62ac88e2c3261b3
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-02-13 20:24:46 -0500

    moved IngredientSafe to its own repository

--- IngredientSafe/homepage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("myButton").onclick = function() {
-    
-    var fname = document.getElementById("myName").value;
-    var allergy = document.getElementById("myAllergy").value
-    console.log(fname);
-    console.log(allergy);
-}
